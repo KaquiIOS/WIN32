e20f57bfb8b7cc1dc94efe4e648706bfcb05ee80
kokoxg2@naver.com <whdtjd1!2@qw>
2024-11-07 17:49:04 +0900

    2024/10/29 중간 커밋 (검색 추가)

--- src/main/kotlin/search/SearchMain.kt
+++ src/main/kotlin/search/SearchMain.kt
@@ -1,33 +1,35 @@
 package org.example.search
 
 import org.example.win32.ext.join
-import org.openqa.selenium.*
+import org.openqa.selenium.By
+import org.openqa.selenium.JavascriptExecutor
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.chrome.ChromeOptions.LOGGING_PREFS
 import org.openqa.selenium.logging.LogType
+import org.openqa.selenium.logging.LoggingPreferences
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.FluentWait
+import org.openqa.selenium.support.ui.WebDriverWait
 import java.io.File
 import java.time.Duration
-import java.util.*
-import kotlin.NoSuchElementException
+import java.util.logging.Level
 
-
-// 1. AnySign.mAnySignEnable 로 페이지 로딩이 되었는지 확인
-// 2. 로딩이 될 때 까지 기다리기
-// 3. ID/PW 입력
-// 4.
 
 
 fun test(corpNo: String) {
 
+    var startTime = System.currentTimeMillis()
+
     System.setProperty("webdriver.chrome.driver", File.separator.join(System.getProperty("user.dir"), "resource", "driver", "chromedriver.130.0.6723.69.exe" ))
 
     val options = ChromeOptions()
+    options.setExperimentalOption("prefs", "profile.managed_default_content_settings.images" to 2)
     options.addArguments("--headless=new")
     options.addArguments("--blink-settings=imagesEnabled=false")
-    options.setCapability(LOGGING_PREFS, mapOf(LogType.BROWSER to "ALL"))
+    options.setCapability(LOGGING_PREFS, mapOf(LogType.BROWSER to Level.ALL))
 
     // WebDriver 초기화
     val driver: WebDriver = ChromeDriver(options)
@@ -37,13 +39,13 @@ fun test(corpNo: String) {
         // 1. Driver Open
         driver.get("http://www.iros.go.kr/PMainJ.jsp")
 
-        // 2. Wait Until AnySign module load
+         // 2. Wait Until AnySign module load
         val wait: FluentWait<WebDriver> = FluentWait(driver)
-            .withTimeout(Duration.ofSeconds(30))
-            .pollingEvery(Duration.ofMillis(150L))
+            .withTimeout(Duration.ofSeconds(5))
+            .pollingEvery(Duration.ofMillis(1000L))
             .ignoring(NoSuchElementException::class.java)
 
-        val isAnySignEnabled = wait.until { (it as JavascriptExecutor).executeScript("return window.AnySign && AnySign.mAnySignEnable === true;") as Boolean }
+        var isAnySignEnabled = wait.until { (it as JavascriptExecutor).executeScript("return window.AnySign && AnySign.mAnySignEnable === true;") as Boolean }
 
         if (!isAnySignEnabled) {
             println("AnySign 모듈이 활성화 되지 않았습니다.")
@@ -87,31 +89,26 @@ fun test(corpNo: String) {
 
         // 6. parse data
         // CSS 선택자로 table 요소가 로드될 때까지 대기
-        val divElement: WebElement = wait.until(ExpectedConditions.presenceOfElementLocated(
-            By.className("list_table")
+        val resultTable: WebElement = wait.until(ExpectedConditions.presenceOfElementLocated(
+            By.cssSelector(".list_table table:nth-of-type(2)")
         ))
 
-        val resultTable = divElement.findElements(By.tagName("table"))
+        val result: List<String> = resultTable.findElements(By.tagName("tr")).drop(1).map { it.text }
 
-        if (resultTable.size > 1) {
-            val searchResult = resultTable.get(1)
+        println(result)
 
-            val rows = searchResult.findElements(By.tagName("tr"))
-
-            if (rows.size > 1) {
-                for (idx in 1 until rows.size) {
-                    val cells = rows[idx].findElements(By.tagName("td"))
-                    println(cells.map { it.text })
-                }
-            }
-        }
     } catch (e: Exception) {
         e.printStackTrace()
     } finally {
+
+        println((System.currentTimeMillis() - startTime) / 1000)
+
         driver.quit()
     }
 }
 
 fun main(args: Array<String>) {
-    test("114271-0001636")
+    for (item in listOf("114271-0001636", "200111-0018882")) {
+        test(item)
+    }
 }
